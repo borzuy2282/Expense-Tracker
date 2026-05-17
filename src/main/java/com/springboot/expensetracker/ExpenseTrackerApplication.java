6e521938fc4dc098d75a7e7926e0e7d814f82b77
Tymofii <tim.barzashvili@icloud.com>
2026-05-17 14:33:09 +0200

    General API information
    Added general api information: title, desc, version, contact info, external documentation

--- src/main/java/com/springboot/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/com/springboot/expensetracker/ExpenseTrackerApplication.java
@@ -1,8 +1,28 @@
 package com.springboot.expensetracker;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Expense Tracker REST API documentation.",
+                description = "Expense Tracker REST API documentation. Implemented using SpringBoot.",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "borzuy2282",
+                        email = "tim.barzashvili@icloud.com",
+                        url = "https://github.com/borzuy2282"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Expense Tracker REST API. Documentation for Developers",
+                url = "https://github.com/borzuy2282/Expense-Tracker"
+        )
+)
 @SpringBootApplication
 public class ExpenseTrackerApplication {
 
