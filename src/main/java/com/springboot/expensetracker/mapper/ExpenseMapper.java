01390d88aedef4e80dcf21fed3ccf3674cf2026c
Tymofii <tim.barzashvili@icloud.com>
2026-05-17 14:17:44 +0200

    Redutant imports
    Cleaned files of redutant imports.

--- src/main/java/com/springboot/expensetracker/mapper/ExpenseMapper.java
+++ src/main/java/com/springboot/expensetracker/mapper/ExpenseMapper.java
@@ -3,7 +3,6 @@ package com.springboot.expensetracker.mapper;
 import com.springboot.expensetracker.dto.ExpenseDto;
 import com.springboot.expensetracker.entity.Expense;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ExpenseMapper {
